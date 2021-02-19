bce7c8ee2cda95d6ee3829bd62d5f17f62424be8
lukehanson2429 <lukehanson2429@gmail.com>
2021-02-19 14:59:36 +0000

    typos

--- js/maps.js
+++ js/maps.js
@@ -22,7 +22,7 @@ function initMap() {
             });
         });
             var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: "https://unpkg.com/@googlemaps/markerclustererplus/dist/index.min.js"
+                imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
             });
 
         }
